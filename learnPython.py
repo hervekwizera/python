57521cef7951d08dd8135ecfd1cbed710b7a74ed
hervekwizera <hervekwizera63@gmail.com>
2025-01-09 08:23:26 +0200

    third commit

--- learnPython.py
+++ learnPython.py
@@ -3,5 +3,13 @@
 
 #Asks the user for their age.Prints: "You are [age] years old."
 
-age = input('enter your age:')
-print(f'you are {age} years old')
+#age = input('enter your age:')
+#print(f'you are {age} years old')
+
+x = 3
+y = 6
+add = x+y
+Subtract = x-y
+multiply = x*y
+divide = x/y
+print(f'add ={add},Subtract = {Subtract},multiply ={multiply}, divide = {divide}')
