b2648e226ae82bf69325aa763776efee9fa4c3b5
hervekwizera <hervekwizera63@gmail.com>
2025-01-09 17:54:11 +0200

    the 4th commit

--- learnPython.py
+++ learnPython.py
@@ -5,7 +5,7 @@
 
 #age = input('enter your age:')
 #print(f'you are {age} years old')
-
+"""
 x = 3
 y = 6
 add = x+y
@@ -13,3 +13,29 @@ Subtract = x-y
 multiply = x*y
 divide = x/y
 print(f'add ={add},Subtract = {Subtract},multiply ={multiply}, divide = {divide}')
+
+Write a program that:
+
+Asks the user for a number.
+Prints whether the number is positive, negative, or zero.
+
+y = float(input('enter a number:'))
+if y > 0 :
+    print('positive')
+elif y < 0 :
+    print('negative')
+else :
+    print('zero')  
+    """  
+""" 
+Exercise 6: Even or Odd
+Write a program that:
+
+Asks the user for a number.
+Prints whether the number is even or odd.
+"""
+num = float(input('enter a number:'))
+if num % 2 == 0 :
+    print('even number')
+else:
+    print('odd number')   
