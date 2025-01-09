6fab576a4eaffba72d19cbed9e9bba8d905eefd4
hervekwizera <hervekwizera63@gmail.com>
2025-01-09 08:07:18 +0200

    second commit

--- learnPython.py
+++ learnPython.py
@@ -1,2 +1,7 @@
-my_name = 'herve' 
-print('my_name:',my_name)
+#my_name = 'herve' 
+#print('my_name:',my_name)
+
+#Asks the user for their age.Prints: "You are [age] years old."
+
+age = input('enter your age:')
+print(f'you are {age} years old')
